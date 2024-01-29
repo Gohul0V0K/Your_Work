1ec5c3c47b2049b3751aca0713dc0389bb316891
Gohul0V0K <136742127+Gohul0V0K@users.noreply.github.com>
2024-01-29 17:37:56 +0000

    dynamic data

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
+Jobs=[
+  {
+    "id":1,
+    "title":"Data Analyst",
+    "location":"Bengaluru, India",
+    "salary":"Rs. 10,00,000"
+  },
+  {
+    "id":2,
+    "title":"Data ",
+    "location":"Bengaluru, India",
+    "salary":"Rs. 10,00,000"
+  },
+  {
+    "id":3,
+    "title":" Analyst",
+    "location":"Bengaluru, India"
+  },
+  {
+    "id":4,
+    "title":"Data enginner",
+    "location":"Bengaluru, India",
+    "salary":"Rs. 10,00,000"
+  }
+]
 @app.route("/")
 def hello():
-  return "Hell"
+  return render_template("home.html",jobs=Jobs)
+  
 
 
 
